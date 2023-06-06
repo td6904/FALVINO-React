2b2a5c31e6e089bfb09525b2cb21e8f4e6677c4d
td6904 <td665pro@pm.me>
2023-06-06 09:10:17 +0200

    Burger menu update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import Mainpage from "./components/Mainpage"
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Mainpage />
+      <Route path='/group' Component={GroupPage} />
     </div>
   );
 }
