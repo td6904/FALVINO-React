60a078fbe136c5e48a35754039351a4bc5e85c90
td6904 <td665pro@pm.me>
2023-06-06 14:05:51 +0200

    Router successfully implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import './App.css';
-import Navbar from "./components/Navbar"
-import Mainpage from "./components/Mainpage"
-import { Route, Routes } from 'react-router-dom';
-import GroupPage from './components/GroupPage';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Mainpage from "./components/Mainpage";
+import GroupPage from "./components/GroupPage";
+import { Routes, Route } from "react-router-dom";
 
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <div>
+//         <Link to="H"
+//       </div>
+//     )
+//   }
+// ])
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-      <Route path='/' component={Mainpage} />
-      <Route path='/group' component={GroupPage} />
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/group" element={<GroupPage />} />
       </Routes>
+
+      {/* <RouterProvider router={router} /> */}
     </div>
   );
 }
